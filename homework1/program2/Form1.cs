ce13315bbf4ef5cf57e9878d27e6788255993c94
潜 周 <522440242@qq.com>
2018-09-11 00:52:42 +0800

    homework1

---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        double a = 0, b = 0, c = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            c = a * b;
+            textBox3.Text = c.ToString();
+        }
+    }
+}
